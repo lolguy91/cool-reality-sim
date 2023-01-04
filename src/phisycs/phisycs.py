4035232d7a159e4c09de2f79bf4f4b087bcbbb9d
lolguy91 <95227041+lolguy91@users.noreply.github.com>
2023-01-04 14:14:59 +0000

    added gravity

--- src/phisycs/phisycs.py
+++ src/phisycs/phisycs.py
@@ -5,15 +5,23 @@ import numpy as np
 
 
 particles = []
-def Coulomb(dist,c1,c2):
-    return 8.98755e11 * ( c1*c2 ) / dist ** 2# !! it returns its result in Newtons
+def CoulombEM(dist,c1,c2):
+    return (8.98755e11 * ( c1*c2 )) / (dist ** 2)# !! it returns its result in Newtons
+def CoulombG(dist,m1,m2):
+    return (6.674e-11 * m1*m2 ) / (dist ** 2)# !! it returns its result in Newtons
 def N2MpS(F,M,dt):
     return (F/M)*dt
-def dist2mag(dist,p1,p2,dt):
-    distconv = dist / 2.25432e-13
-    value = N2MpS(Coulomb(distconv,p1.charge,p2.charge),p1.mass,dt)# its in meters per second
+def dist2magEM(dist,p1,p2,dt):
+    distconv = dist * 4435927463714.113
+    value = N2MpS(CoulombEM(distconv,p1.charge,p2.charge),p1.mass,dt)# its in meters per second
     
-    return (value * 2.25432e-13) / dt# our scale
+    return (value / 4435927463714.113) / dt# our scale
+#                   ^space scale   ^time scale
+def dist2magG(dist,p1,p2,dt):
+    distconv = dist * 4435927463714.113
+    value = N2MpS(CoulombG(distconv,p1.mass,p2.mass),p1.mass,dt)# its in meters per second
+    
+    return (value / 4435927463714.113) / dt# our scale
 #                   ^space scale   ^time scale
 
 def normalize(v):
@@ -31,7 +39,22 @@ def applyEM(x,x2,y,y2, p1,p2,dt):
     
     dirx,diry = normalize((xdiff,ydiff))
     
-    mag = dist2mag(dist,p1,p2,dt)
+    mag = dist2magEM(dist,p1,p2,dt)
+    
+    return (dirx * mag,diry * mag)
+
+def applyG(x,x2,y,y2, p1,p2,dt):
+
+    xdiff = x2 - x
+    ydiff = y2 - y
+    
+    if xdiff == 0 or ydiff == 0 : return (0,0)
+    
+    dist = math.dist([x,y], [x2,y2])
+    
+    dirx,diry = normalize((xdiff,ydiff))
+    
+    mag = dist2magG(dist,p1,p2,dt)
     
     return (dirx * mag,diry * mag)
 
@@ -45,8 +68,7 @@ def mkneutron(x,y,_startvel = (0,0)):
     particles.append(particle.Particle((x,y),20,0,1.67262e-27,_startvel))
     
 def init():
-    add_hydrogen(200,0)
-    add_hydrogen(-200,0)
+    add_hydrogen(0,0)
     
 
 def add_hydrogen(ox,oy):
@@ -59,9 +81,6 @@ def calculate_orbit_velocity(x,y):
     a = np.abs(c)
     c /= a
     angle = np.log(c) / (2j * np.pi)
-    angle += a / 777
-    c = a * np.exp(2j * np.pi * angle)
-    nextpos = (np.real(c), np.imag(c))
     vel = 10
     return (math.sin(angle) * vel,math.cos(angle) * vel)
     
@@ -74,7 +93,10 @@ def update(dt):
             if particle2 == particle: continue
             
             x2,y2 = particle2.coords
-            particle.velocity = (particle.velocity[0] - applyEM(x,x2,y,y2,particle,particle2,dt)[0],particle.velocity[1] - applyEM(x,x2,y,y2,particle,particle2,dt)[1])
+            
+            em = applyEM(x,x2,y,y2,particle,particle2,dt)
+            g =  applyG(x,x2,y,y2,particle,particle2,dt)
+            particle.velocity = (particle.velocity[0] - em[0] - g[0],particle.velocity[1] - em[1] - g[1])
     
         particle.coords = (particle.coords[0] + (particle.velocity[0] * dt),particle.coords[1] + (particle.velocity[1] *dt))# python fucking sucks
         
